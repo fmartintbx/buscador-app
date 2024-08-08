3b37062d69e96fa7d6c45dc42e0c11598eca6d78
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-08-08 13:13:47 -0300

    useState

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
 
 
 export default function BuscadorPeliculas() {
+  
+  const [busqueda, setBusqueda] = useState('')
+  
+  const handleInputChange = (e) => {
+    setBusqueda(e.target.value)
+  }
   return (
-    <div>BuscadorPeliculas</div>
+    <div className="container">
+      <h1 className="title">Buscador de Peliculas</h1>
+      <form> 
+        <input type="text"
+        placeholder="Escribi una peliicula" 
+        value={busqueda}
+        onChange={handleInputChange}
+        
+        />
+       <button type= "submit"  className="search-button">Buscar</button>
+      </form>
+    </div>
   )
 }
