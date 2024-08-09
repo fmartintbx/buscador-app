419e0d2d9a82f0265e5c6c33bc56667012c3c625
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-08-09 11:35:28 -0300

    Buscador-app

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -3,15 +3,33 @@ import { useState } from "react"
 
 export default function BuscadorPeliculas() {
   
+  const urlBase= 'http:'
+
   const [busqueda, setBusqueda] = useState('')
+  const [peliculas, setPeliculas] = useState([])
   
   const handleInputChange = (e) => {
     setBusqueda(e.target.value)
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchPeliculas()
+  }
+  const fetchPeliculas = async() => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query=${busqueda}`)
+      const data = await response.json()
+      setPeliculas(data.results)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de Peliculas</h1>
-      <form> 
+      <form onSubmit={handleSubmit}> 
         <input type="text"
         placeholder="Escribi una peliicula" 
         value={busqueda}
